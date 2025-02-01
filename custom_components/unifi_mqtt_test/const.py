2aaec634fcb99609030164839220ffd81ac8890b
ingoldsby <jim@kneebone.com.au>
2025-02-01 19:23:12 +1000

    Create const.py

---
+++ custom_components/unifi_mqtt_test/const.py
@@ -0,0 +1,18 @@
+"""Constants for the UniFi MQTT integration."""
+
+DOMAIN = "unifi_mqtt_test"
+
+CONF_HOST = "host"
+CONF_USERNAME = "username"
+CONF_PASSWORD = "password"
+CONF_SITE_ID = "site_id"
+CONF_PORT = "port"
+CONF_VERIFY_SSL = "verify_ssl"
+CONF_VERSION = "version"
+
+DEFAULT_SITE_ID = "default"
+DEFAULT_PORT = 443
+DEFAULT_VERIFY_SSL = True
+DEFAULT_VERSION = "UDMP-unifiOS"
+
+UPDATE_INTERVAL = 300  # seconds (5 minutes)
