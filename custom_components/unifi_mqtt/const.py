48b1b6e32ff64efaad40e01c90a9807749b60318
ingoldsby <jim@kneebone.com.au>
2025-02-03 20:26:30 +1000

    Update and rename const.py to const.py

--- custom_components/unifi_mqtt/const.py
+++ custom_components/unifi_mqtt/const.py
@@ -1,6 +1,6 @@
 """Constants for the UniFi MQTT integration."""
 
-DOMAIN = "unifi_mqtt_test"
+DOMAIN = "unifi_mqtt"
 
 CONF_HOST = "host"
 CONF_USERNAME = "username"
@@ -9,7 +9,6 @@ CONF_SITE_ID = "site_id"
 CONF_PORT = "port"
 CONF_VERIFY_SSL = "Verify SSL"
 CONF_VERSION = "version"
-CONF_UPDATE_INTERVAL = "Update interval"
 
 VERSION_OPTIONS = [
     "UDMP-unifiOS",  # default
@@ -22,4 +21,5 @@ DEFAULT_SITE_ID = "default"
 DEFAULT_PORT = 443
 DEFAULT_VERIFY_SSL = False
 DEFAULT_VERSION = "UDMP-unifiOS"
-DEFAULT_UPDATE_INTERVAL = 60 # seconds
+
+UPDATE_INTERVAL = 60  # seconds
